9a6f7e3eb468281c4d6536adcb2786bad5058561
limengkai0225 <limengkai0225@live.cn>
2020-06-30 07:28:18 +0800

    1

---
+++ helloFX/src/main/java/com/limengkai0225/dev/AppLauncher.java
@@ -0,0 +1,13 @@
+package com.limengkai0225.dev;
+
+import javafx.application.Application;
+
+public class AppLauncher {
+    public static void main(String [] args){
+        Application.launch(JavaApplication.class,args);
+    }
+}
+/*
+
+
+ */
