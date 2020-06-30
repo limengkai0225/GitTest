a6e96838c19970a8d8ee04457cdfeae927491fb6
limengkai0225 <limengkai0225@live.cn>
2020-06-30 23:06:57 +0800

    aaa

--- helloFX/src/main/java/com/limengkai0225/dev/AppLauncher.java
+++ helloFX/src/main/java/com/limengkai0225/dev/AppLauncher.java
@@ -7,6 +7,7 @@ public class AppLauncher {
         Application.launch(JavaApplication.class,args);
         //feature branch download
         //feature branch download2
+		//feature branch update1
     }
 }
 /*
